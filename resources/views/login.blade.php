8744b9d247a272c263b46fa282d795ff81ceb291
youssef elqayedy <elqayedycontact@gmail.com>
2024-07-28 18:57:34 +0100

    styling the login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,42 +6,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Fidelis - Login</title>
-    <style>
-        form {
-            width: fit-content;
-        }
-
-        .error {
-            background-color: rgba(247, 105, 105, 0.464);
-            color: red;
-            text-align: center;
-            margin-bottom: 20px;
-            font-weight: bold;
-            padding: 5px 0;
-        }
-    </style>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('dist/css/log_in.css') }}">
 </head>
 
 <body>
-    <h1>Login page</h1>
-    <form action={{ route('login') }} method="POST">
-        @csrf
-
-        @error('error')
-            <div class="error">
-                {{ $message }}
+    @error('error')
+        <div class="message"><span>{{ $message }}</span><button class="close_error">x</button></div>
+    @enderror
+    <div class="log_in">
+        <div class="head">
+            <img class="logo" src="{{ asset('img/logo.png') }}" alt="fediles logo">
+            <h1>Log in</h1>
+        </div>
+        <form class="form" action={{ route('login') }} method="POST">
+            @csrf
+            <div>
+                <label for="email">email</label>
+                <input type="text" id="email" name="email" autofocus required>
             </div>
-        @enderror
+            <div>
+                <label for="password">password</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button>connect</button>
+            {{-- <a href={{ route('forgetPassword.show') }}>forget password</a> --}}
+        </form>
+    </div>
 
-        <label for="email">Email: </label>
-        <input type="text" id="email" name="email" autofocus>
-        <br>
-        <label for="password">Password: </label>
-        <input type="text" id="password" name="password">
-        <br>
-        <button>Login</button>
-        {{-- <a href={{ route('forgetPassword.show') }}>forget password</a> --}}
-    </form>
+    <script>
+        window.addEventListener('load', () => {
+            const messages = document.querySelectorAll('.message');
+            const messagesCloseError = document.querySelectorAll('.message .close_error');
+
+            messages.forEach(e => (e?.classList.add('show'), console.log(e)));
+
+            messagesCloseError.forEach(btn => {
+                btn.onclick = () => {
+                    messages.forEach(parent => {
+
+                        if (parent.contains(btn)) parent.classList.remove('show');
+
+                    })
+                };
+
+            });;
+        });
+    </script>
 
 </body>
 
