df86a9e0b7ef6acbce039719ad822a742f271cef
Youssef ElQayedy <elqayedycontact@gmail.com>
2024-08-05 15:47:21 +0100

    done

--- resources/views/pages/dashboard/cards/cards.blade.php
+++ resources/views/pages/dashboard/cards/cards.blade.php
@@ -12,28 +12,27 @@
             <div class="title">cards</div>
             <a class="add" href=""> <i class="fa-solid fa-plus"></i><span>add new card</span></a>
         </div>
-        <div class="cards">
-            @for ($i = 0; $i < 15; $i++)
-                <div class="card">
-                    <img class="bg-img" src="{{ asset('img/card1.png') }}" alt="">
-                    <div class="info">
-                        <div>
-                            <div class="type-of-card">Gold Card</div>
-                            <img class="qr-code" src="{{ asset('img/qr_code.png') }}" alt="">
-                        </div>
-                        <div>
-                            <div class="client">
-                                <div class="name">youssef elqayedy</div>
-                                <div class="serial"><span>5195</span><span>1495</span><span>1989</span><span>1561</span></div>
+        <div class="main-table">
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th class="actions">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row">0</td>
+                        <td>
+                            <div class="actions">
+                                <a href=>
+                                    <span>-</span>
+                                </a>
                             </div>
-                            <div class="expiry-date">
-                                <div class="expiry">expiry</div>
-                                <div class="date">07/30</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endfor
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </section>
 @endsection
