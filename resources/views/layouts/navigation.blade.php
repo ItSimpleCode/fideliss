93b7050f77ac364e7dc9503829ae438d87893018
Yasser <fahmiyasser03@gmail.com>
2024-08-13 13:58:39 +0100

    Adding dark mode treatement

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,19 +5,31 @@
             <span class="gender">
                 @if (Auth::guard('admin')->check())
                     <span>{{ Auth::guard('admin')->user()->gender == 'male' ? 'mr.' : 'mme.' }}</span>
-                    <span>{{ Auth::guard('admin')->user()->first_name }} {{ Auth::guard('admin')->user()->last_name }}</span>
+                    <span>{{ Auth::guard('admin')->user()->first_name }}
+                        {{ Auth::guard('admin')->user()->last_name }}</span>
                 @elseif(Auth::guard('staff')->check())
                     <span>{{ Auth::guard('staff')->user()->gender == 'male' ? 'mr.' : 'mme.' }}</span>
-                    <span>{{ Auth::guard('staff')->user()->first_name }} {{ Auth::guard('staff')->user()->last_name }}</span>
+                    <span>{{ Auth::guard('staff')->user()->first_name }}
+                        {{ Auth::guard('staff')->user()->last_name }}</span>
                 @endif
             </span>
         </span>
+        {{ session('theme') }}
     </div>
     @yield('search')
     <div class="nav-options">
         @yield('date')
         {{-- <button class="square-btn"><span>99</span><i class="fa-regular fa-bell"></i></button> --}}
         {{-- <button class="square-btn"><span>99</span><i class="fa-regular fa-message"></i></button> --}}
-        <button class="square-btn"><i class="fa-regular fa-lightbulb"></i></button>
+        @if (session('theme') == 'dark')
+            <a href={{ route('changeTheme', ['theme' => 'light']) }} class="square-btn">
+                <i class="fa-regular fa-lightbulb"></i>
+            </a>
+        @else
+            <a href={{ route('changeTheme', ['theme' => 'dark']) }} class="square-btn">
+                <i class="fa-solid fa-moon"></i>
+            </a>
+        @endif
+
     </div>
 </nav>
